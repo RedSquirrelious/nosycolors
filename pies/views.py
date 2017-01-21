7a66f1fe147d58bb0f44a65154dc2549a15aba5c
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-20 16:10:32 -0800

    changed query method to pass in an array of words instead of querying the db for each word, sped up max sorting

--- pies/views.py
+++ pies/views.py
@@ -40,15 +40,8 @@ punct = list(string.punctuation)
 stopword_list = stopwords.words('english') + punct + ['rt', 'via', '...', '…']
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
-
-class AboutView(TemplateView):
-    template_name = "about.html"
-
-
 # test_tweet1 = "@ZeroUtopia @democracynow Unfortunately his listening skills are not good enough for that level of meatiness. I'll probably do @BBC first"
-# test_tweet2 = "RT @SUPGVNetwork: A toddler has now shot a person every week in America for two years straight. Yes, you read that correctly. https://t.co…"
+test_tweet2 = "RT @SUPGVNetwork: A toddler has now shot a person every week in America for two years straight. Yes, you read that correctly. https://t.co…"
 # test_tweet3 = "@ZeroUtopia I had to just grit my teeth and repeat in my head ... still better than FOX... still better than FOX.... UGH"
 # test_tweet4 = "RT @quasimado: A quarter of 2016's mass shootings occurred when a woman was attempting to leave a relationship https://t.co/oOxZR7Ztth"
 # test_tweets = [test_tweet1, test_tweet2, test_tweet3, test_tweet4]
@@ -61,6 +54,12 @@ class AboutView(TemplateView):
 
 # 	return render(request, 'pie_data.html', context)
 
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
 
 
 # NEEDED FOR CLASSIFY
@@ -72,72 +71,68 @@ def process(text, tokenizer=TweetTokenizer(), stopwords=[]):
 	return [tok for tok in tokens if tok not in stopword_list and not tok.isdigit()]
 
 
-
-
 # NEEDED FOR CLASSIFY
-def query_emolex(host, database, user, password, tweet_word):
+def query_emolex(host, database, user, password, tweet):
 
 	cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
 	cursor = cnx.cursor(dictionary=True)
-
+	tweet_words = process(tweet)
 # make sure tweets don't break the database
-	regex = re.compile('[^a-zA-Z/\s]')
-	clean_tweet_word = regex.sub('', tweet_word)
-	
-	query = "SELECT w.word, e.emotion, w.count, wes.score, wes.word_id, wes.emotion_id, w.id, e.id FROM words w JOIN word_emotion_score wes ON wes.word_id = w.id JOIN emotions e ON e.id = wes.emotion_id WHERE w.word = %s"
+	regex = re.compile('[^a-zA-Z]')
+	# print(tweet_words)
+	for i in range(len(tweet_words)):
+		tweet_words[i] = regex.sub('', tweet_words[i])
 
-	cursor.execute(query, [clean_tweet_word])
+	# print(tweet_words)
+	query = "SELECT w.word, e.emotion, w.count, wes.score, wes.word_id, wes.emotion_id, w.id, e.id FROM words w JOIN word_emotion_score wes ON wes.word_id = w.id JOIN emotions e ON e.id = wes.emotion_id WHERE w.word IN (%s)"
+ 
+	in_p=', '.join(list(map(lambda x: '%s', tweet_words)))
+
+	query = query % in_p
+	# print(query)
+	cursor.execute(query, tuple(tweet_words))
 
 	results = cursor.fetchall()
-
-	emotion_list = []
-
-	for emotion in results:
-		average_score = emotion['score']/emotion['count']
-		emotion_w_score = (emotion['emotion'], average_score)
-		emotion_list.append(emotion_w_score)
-
-	return emotion_list
-
+	# print(results)
 
 	cursor.close()
 	cnx.close()
 
+	emotion_list = dict()
 
+	for word in results:
+		if word['word'] not in emotion_list:
+			emotion_list[word['word']] = []
+		average_score = word['score']/word['count']
+		emotion_w_score = (word['emotion'], average_score)
+		# emotion_list.append(emotion_w_score)	
+		emotion_list[word['word']].append(emotion_w_score)
 
-
-# NEEDED FOR CLASSIFY
-def find_strongest_emotion_for_word(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, tweet_word):
-
-	emotion_list = query_emolex(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, tweet_word)
-	final_scoring = []
-	if emotion_list:
-		highest_scoring_emotion = max(emotion_list, key=itemgetter(1))[0]
-		highest_score = max(emotion_list, key=itemgetter(1))[1]
-		final_scoring.append(highest_scoring_emotion)
-		final_scoring.append(highest_score)
-		return final_scoring
-
-
-
-
-
-# NEEDED FOR CLASSIFY 
-def find_strongest_emotions_in_tweet(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, word_list):
-	emotion_list = []
-	
-	for word in word_list:
-		highest_scoring_emotion = find_strongest_emotion_for_word(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, word)
-
-		if highest_scoring_emotion:
-			emotion_list.append(highest_scoring_emotion)
-	
 	return emotion_list
 
-def show_top_emotion(emotion_array):
+
+
+def find_strongest_emotions_in_tweet(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, tweet):
+
+	emotion_list = query_emolex(HOST, DATABASE_NAME, USER_NAME, DATABASE_KEY, tweet)
+	final_scoring = dict()
+
+	for word in emotion_list:
+		highest_scoring_emotion = max(emotion_list[word], key=itemgetter(1))[0]
+		highest_score = max(emotion_list[word], key=itemgetter(1))[1]
+
+		final_scoring[word] = (highest_scoring_emotion, highest_score)
+	print(final_scoring)
+	return final_scoring
+
+
+
+
+def show_top_emotion(emotion_dictionary):
 	emotion_hash = {"anger": 0, "anticipation": 0, "disgust": 0, "fear": 0, "joy": 0, "sadness": 0, "surprise": 0, "trust": 0}
 
-	for emotion in emotion_array:
+	for word in emotion_dictionary:
+		emotion = emotion_dictionary[word]
 		if emotion[0] == 'anger':
 			emotion_hash['anger'] += 1
 		if emotion[0] == 'anticipation':
@@ -156,57 +151,55 @@ def show_top_emotion(emotion_array):
 			emotion_hash['trust'] += 1
 	return emotion_hash.items()
 
-
-
 #THIS ONE IS IN PROGRESS
-def get_pie_data(request):
+# def get_pie_data(request):
 
-	if request.method == 'POST':
-	    # create a form instance and populate it with data from the request:
-		form = HandleForm(request.POST)
+# 	if request.method == 'POST':
+# 	    # create a form instance and populate it with data from the request:
+# 		form = HandleForm(request.POST)
 
-		if form.is_valid():
+# 		if form.is_valid():
 
-			target_handle = form.cleaned_data['target_handle']
-			number_of_tweets = form.cleaned_data['number_of_tweets']
+# 			target_handle = form.cleaned_data['target_handle']
+# 			number_of_tweets = form.cleaned_data['number_of_tweets']
 			
-			rawtweepy = settings.AUTHORIZED_USER.user_timeline(screen_name=target_handle, count=number_of_tweets)
+# 			rawtweepy = settings.AUTHORIZED_USER.user_timeline(screen_name=target_handle, count=number_of_tweets)
 
 				
-			user = settings.AUTHORIZED_USER.get_user(screen_name=target_handle)
-			target = dict()
-			target['name'] = user.name
-			target['screen_name'] = user.screen_name
+# 			user = settings.AUTHORIZED_USER.get_user(screen_name=target_handle)
+# 			target = dict()
+# 			target['name'] = user.name
+# 			target['screen_name'] = user.screen_name
 
-			tweets = []
+# 			tweets = []
 
-			for raw_tweet in rawtweepy:
-				tweet = {}
+# 			for raw_tweet in rawtweepy:
+# 				tweet = {}
 
-				tweet_words = process(raw_tweet.text)
+# 				tweet_words = process(raw_tweet.text)
 				
-				strongest_emotions = find_strongest_emotions_in_tweet(settings.HOST, settings.DATABASE_NAME, settings.USER_NAME, settings.DATABASE_KEY, tweet_words)
+# 				strongest_emotions = find_strongest_emotions_in_tweet(settings.HOST, settings.DATABASE_NAME, settings.USER_NAME, settings.DATABASE_KEY, tweet_words)
 
-				if strongest_emotions:
-					# strongest_emotions = ", ".join(strongest_emotions)
-					tweet['emotion'] = strongest_emotions
-				else:
-					tweet['emotion'] = 'Unable to process this tweet'
+# 				if strongest_emotions:
+# 					# strongest_emotions = ", ".join(strongest_emotions)
+# 					tweet['emotion'] = strongest_emotions
+# 				else:
+# 					tweet['emotion'] = 'Unable to process this tweet'
 
-				tweet['text'] = (raw_tweet.text)
+# 				tweet['text'] = (raw_tweet.text)
 
-				tweets.append(tweet)
-
-
-
-			context = {'target_handle': target_handle, 'tweets': tweets, 'target': target }
-
-# if a GET (or any other method) we'll create a blank form
-	else:
-		form = HandleForm()
+# 				tweets.append(tweet)
 
 
-	return context
+
+# 			context = {'target_handle': target_handle, 'tweets': tweets, 'target': target }
+
+# # if a GET (or any other method) we'll create a blank form
+# 	else:
+# 		form = HandleForm()
+
+
+# 	return context
 
 
 #THIS ONE WORKS
@@ -224,6 +217,7 @@ def pie_data(request):
 
 				
 			user = settings.AUTHORIZED_USER.get_user(screen_name=target_handle)
+
 			target = dict()
 			target['name'] = user.name
 			target['screen_name'] = user.screen_name
@@ -239,12 +233,12 @@ def pie_data(request):
 				tweet = {}
 				tweet['text']= test_tweet.text
 
-				first_pass = process(test_tweet.text)
+				# first_pass = process(test_tweet.text)
 				# print(first_pass)
-				emotions = find_strongest_emotions_in_tweet(settings.HOST, settings.DATABASE_NAME, settings.USER_NAME, settings.DATABASE_KEY, first_pass)
+				emotions = find_strongest_emotions_in_tweet(settings.HOST, settings.DATABASE_NAME, settings.USER_NAME, settings.DATABASE_KEY, test_tweet.text)
 				# print(emotions)
-				count = list(show_top_emotion(emotions))
-				# print(count)
+				count = show_top_emotion(emotions)
+				print(count)
 				for emotion in count:
 					if emotion[1] > 0:
 						one_emotion_list.append(emotion[0])
