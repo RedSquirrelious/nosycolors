e93648a2153a8753db9cca41d769d3bc68889967
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-23 17:02:56 -0800

    removed last of settings. variables

--- nosycolors/settings.py
+++ nosycolors/settings.py
@@ -23,15 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DATABASE_NAME = os.environ['DATABASE_NAME']
-DATABASE_KEY = os.environ['DATABASE_KEY']
-USER_NAME = os.environ['USER_NAME']
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
-HOST = os.environ['HOST']
-CALLBACK_URL = os.environ['CALLBACK_URL']
+
 
 # *************************************
 # NOT SURE IF THIS BELONGS HERE??
