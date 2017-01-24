23bb33fa28d51576b94b34a9457005fa7c205c9f
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-23 17:56:32 -0800

    secret key?

--- nosycolors/settings.py
+++ nosycolors/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # *************************************
 # NOT SURE IF THIS BELONGS HERE??
