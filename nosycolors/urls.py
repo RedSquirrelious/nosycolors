ee3f77ecaab871d4dd54de46ed4bbaffdcbc57a3
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-24 16:29:39 -0800

    urls

--- nosycolors/urls.py
+++ nosycolors/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
 		# url(r'^$', 'pies.views.index'),
 		url(r'^admin/', admin.site.urls),
-		url(r'^/', include('pies.urls')),
+		url(r'^', include('pies.urls')),
 ]
